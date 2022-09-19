8ca687874e6a4289518ec42a99a92ec81b653831
Marcus Carvalho <marcusca@outlook.com>
2022-09-19 13:23:05 +0200

    simplified FBA configuration

--- AdfsActivity/Private/Get-LoginFormFields.ps1
+++ AdfsActivity/Private/Get-LoginFormFields.ps1
@@ -21,16 +21,10 @@ function Get-LoginFormFields {
     $user = $Credential.UserName
     $password = ConvertFrom-SecureString $Credential.Password
 
-    Write-Verbose "Performing pre login activity for '$($user))'"
-    # initial web request to get the login web form
-    $preLogin = Invoke-WebRequest -Uri $Url -ErrorAction SilentlyContinue
-    if ($preLogin.StatusCode -ne 200) { Write-Error "HTTP request for pre login failed with status $($preLogin.StatusCode) for user: $($user)" -ErrorAction Stop }
+    $fields = New-Object -TypeName "System.Collections.Generic.Dictionary[string,string]"
+    $fields.Add("UserName",$user)
+    $fields.Add("Password",$password)
+    $fields.Add("AuthMethod","FormsAuthentication")
 
-    # set web form values
-    $form = $preLogin.Forms[0]
-    $form.Fields["UserName"] = $user
-    $form.Fields["Password"] = $password
-    $form.Fields["AuthMethod"] = "FormsAuthentication"
-
-    return $form.Fields
+    return $fields # $form.Fields
 }
