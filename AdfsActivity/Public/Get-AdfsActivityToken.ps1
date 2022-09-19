61e49f9ca1c54cb823a3e4677a53f6482535ef86
Marcus Carvalho <marcusca@outlook.com>
2022-09-19 11:59:57 +0200

    Added support for WS-Trust
    Added support for WS-Trust and set it as default protocol

--- AdfsActivity/Public/Get-AdfsActivityToken.ps1
+++ AdfsActivity/Public/Get-AdfsActivityToken.ps1
@@ -5,15 +5,22 @@
     This command will acquire OAuth tokens for both public and confidential clients. Public clients authentication can be interactive, integrated Windows auth, or silent (aka refresh token authentication).
 .EXAMPLE
     PS C:\>Get-AdfsActivityToken urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com
-    Sign in to an application on an AD FS server using logged user credentials.
+    Sign in to an application on an AD FS server using logged user credentials using the WindowsTransport endpoint.
 .EXAMPLE
     PS C:\>$credential = Get-Credential
-    PS C:\>Get-AdfsActivityToken -Application urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com - $credential
-    Sign in  to an application on an AD FS server using credentials provided by the user.
+    PS C:\>Get-AdfsActivityToken urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com -Credential $credential
+    Sign in  to an application on an AD FS server using credentials provided by the user using the UserNameMixed endpoint.
+.EXAMPLE
+    PS C:\>Get-AdfsActivityToken urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com -Protocol WsFed
+    Sign in to an application on an AD FS server using logged user credentials using the Ws-Fed protocol.
+.EXAMPLE
+    PS C:\>$credential = Get-Credential
+    PS C:\>Get-AdfsActivityToken urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com -Protocol WsFed
+    Sign in  to an application on an AD FS server using credentials provided by the user using the Ws-Fed endpoint and forms based authentication.
 .EXAMPLE
     PS C:\>$password = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
     PS C:\>$credential = New-Object PSCredential ('user2@contoso.com', $password)
-    PS C:\>Start-AdfsActivity -Application urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com - $credential
+    PS C:\>Get-AdfsActivityToken urn:federation:MicrosoftOnline -AdfsHost adfs.contoso.com -Credential $credential
     Sign in  to an application on an AD FS server using credentials passed via script.
 .EXAMPLE
     PS C:\>$SamlIdentifiers =  Get-AdfsRelyingPartyTrust | where { $_.WSFedEndpoint -eq $null } | foreach { $_.Identifier.Item(0) }
@@ -39,36 +46,69 @@ function Get-AdfsActivityToken
       HelpMessage = 'Enter host name for the AD FS server')]
     [string]$AdfsHost,
     [Parameter(Mandatory=$false,
-      HelpMessage = 'Select the protocol for the application, SAML or WsFed (valid default is WsFed)')]
-    [ValidateSet("SAML", "WsFed")]
-    [string]$Protocol = "WsFed",
+      HelpMessage = 'Select the protocol for the application: WsTrust, SAML or WsFed (valid default is WsTrust)')]
+    [ValidateSet("SAML", "WsFed", "WsTrust")]
+    [string]$Protocol = "WsTrust",
     [Parameter(Mandatory=$false,
       HelpMessage = 'Provide the credential for the user to be signed in.')]
     [pscredential]$Credential
   )
 
-  if ($Protocol -eq "SAML") {
-    $samRequest = New-AdfsActivitySamlRequestB64 $Identifier
-    $uri = "https://$($AdfsHost)/adfs/ls?SAMLRequest=$([uri]::EscapeDataString($samRequest))"
-  }
-  else {
-    # Defaults to Ws-Fed request
-    $uri = "https://$($AdfsHost)/adfs/ls?client-request-id=$(New-Guid)&wa=wsignin1.0&wtrealm=$($Identifier)"
+  if ($null -ne $Credential) 
+  {
+    Write-Warning "Using credentials sends password in clear text over the network!"
   }
 
-  if ($null -ne $Credential) {
-    $user = $Credential.UserName
-    $form = Get-LoginFormFields -Url $uri -Credential $Credential
-    $login = Invoke-WebRequest -Uri $uri -Method POST -Body $form -UseBasicParsing -ErrorAction SilentlyContinue
+
+  if ($Protocol -eq "WsTrust") {
+    if ($null -ne $Credential) {
+      $user = $Credential.UserName
+      $uri = "https://$($AdfsHost)/adfs/services/trust/2005/usernamemixed"
+
+      $wstrustRequest = New-AdfsActivityWsTrustRequest $Identifier -Endpoint $uri -Credential $Credential
+      $login = Invoke-WebRequest $uri -Method Post -Body $wstrustRequest -ContentType "application/soap+xml" -UseBasicParsing -ErrorAction SilentlyContinue
+    }
+    else {
+      $uri = "https://$($AdfsHost)/adfs/services/trust/2005/windowstransport"
+      $user = "$($env:USERDOMAIN)\$($env:UserName)"
+
+      $wstrustRequest = New-AdfsActivityWsTrustRequest $Identifier -Endpoint $uri
+      $login = Invoke-WebRequest $uri -Method Post -Body $wstrustRequest -ContentType "application/soap+xml" -UseDefaultCredentials -UseBasicParsing -ErrorAction SilentlyContinue
+    }
   }
   else {
-    $userAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT; Windows NT 10.0; en-US)'
-    $user = "$($env:USERDOMAIN)\$($env:UserName)"
-    $login = Invoke-WebRequest -Uri $uri -UserAgent $userAgent -UseDefaultCredentials -UseBasicParsing -ErrorAction SilentlyContinue
+    if ($Protocol -eq "SAML") {
+      $samRequest = New-AdfsActivitySamlRequestB64 $Identifier
+      $uri = "https://$($AdfsHost)/adfs/ls?SAMLRequest=$([uri]::EscapeDataString($samRequest))"
+    }
+    else {
+      # Defaults to Ws-Fed request
+      $uri = "https://$($AdfsHost)/adfs/ls?client-request-id=$(New-Guid)&wa=wsignin1.0&wtrealm=$($Identifier)"
+    }
+    
+    if ($null -ne $Credential) {
+      $user = $Credential.UserName
+      $form = Get-LoginFormFields -Url $uri -Credential $Credential
+      $login = Invoke-WebRequest -Uri $uri -Method POST -Body $form -UseBasicParsing -ErrorAction SilentlyContinue
+    }
+    else {
+      $userAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT; Windows NT 10.0; en-US)'
+      $user = "$($env:USERDOMAIN)\$($env:UserName)"
+      $login = Invoke-WebRequest -Uri $uri -UserAgent $userAgent -UseDefaultCredentials -UseBasicParsing -ErrorAction SilentlyContinue
+    }
   }
 
-  if ($login.StatusCode -ne 200) { Write-Error "HTTP request failed with status $($login.StatusCode) for identifier ""$($identifier)"" and user: $($user)" }
-  elseif ($login.InputFields.Count -le 0) { Write-Warning "Login failed for user: $($user)"}
+
+
+  if ($null -eq $login) { Write-Error "HTTP request failed for identifier ""$($identifier)"" and user: $($user)" }
+  elseif ($login.StatusCode -ne 200) { Write-Error "HTTP request failed with status $($login.StatusCode) for identifier ""$($identifier)"" and user: $($user)" }
+  elseif ($login.InputFields.Count -le 0) {
+    if ($login.Headers["Content-Type"].Contains("application/soap+xml")) {
+      Write-Host "Login sucessful for identifier ""$($Identifier)"" and user: $($user) (Ws-Trust)"
+      return $login.Content
+    }
+    else { Write-Warning "Login failed for user: $($user)" }
+  }
   elseif ($login.InputFields[0].outerHTML.Contains("wsignin1.0")) {
     Write-Host "Login sucessful for identifier ""$($Identifier)"" and user: $($user) (Ws-Fed)"
     return $login.Content
---
+++ AdfsActivity/Public/New-AdfsActivityWsTrustRequest.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    Create a WS-Trust request.
+.EXAMPLE
+    PS C:\>New-AdfsActivityWsTrustRequest urn:federation:MicrosoftOnline
+    Create a Ws-Trust request for the application urn:federation:MicrosoftOnline.
+#>
+function New-AdfsActivityWsTrustRequest {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        # Application identifier
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string] $Identifier,
+        [Parameter(Mandatory=$true,
+            HelpMessage = 'Enter Ws-Trust endpoint.')]
+        [string]$Endpoint,
+        [Parameter(Mandatory=$false,
+            HelpMessage = 'Provide the credential for the user to be signed in.')]
+        [pscredential]$Credential
+    )
+
+    if ($credential -ne $null)
+    {
+        $username = $Credential.UserName
+        $password = ConvertFrom-SecureString $Credential.Password
+        $request = [String]::Format(
+            '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</a:Action><a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">{0}</a:To><o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><o:UsernameToken u:Id="uuid-52bba51d-e0c7-4bb1-8c99-6f97220eceba-5"><o:Username>{1}</o:Username><o:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">{2}</o:Password></o:UsernameToken></o:Security></s:Header><s:Body><t:RequestSecurityToken xmlns:t="http://schemas.xmlsoap.org/ws/2005/02/trust"><wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy"><a:EndpointReference><a:Address>{3}</a:Address></a:EndpointReference></wsp:AppliesTo><t:KeySize>0</t:KeySize><t:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</t:KeyType><t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType><t:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</t:TokenType></t:RequestSecurityToken></s:Body></s:Envelope>', `
+                $endpoint,
+                $username,
+                $password,
+                $Identifier)
+    }
+    else
+    {
+        $request = [String]::Format(
+            '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://www.w3.org/2005/08/addressing" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><a:Action s:mustUnderstand="1">http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</a:Action><a:ReplyTo><a:Address>http://www.w3.org/2005/08/addressing/anonymous</a:Address></a:ReplyTo><a:To s:mustUnderstand="1">{0}</a:To></s:Header><s:Body><t:RequestSecurityToken xmlns:t="http://schemas.xmlsoap.org/ws/2005/02/trust"><wsp:AppliesTo xmlns:wsp="http://schemas.xmlsoap.org/ws/2004/09/policy"><a:EndpointReference><a:Address>{1}</a:Address></a:EndpointReference></wsp:AppliesTo><t:KeySize>0</t:KeySize><t:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</t:KeyType><t:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</t:RequestType><t:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</t:TokenType></t:RequestSecurityToken></s:Body></s:Envelope>', `
+                $endpoint,
+                $Identifier)
+    }
+
+    return $request
+}
